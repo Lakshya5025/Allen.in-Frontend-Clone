200f016268824b70ce8a8ffaafe01c52b5d6ce4c
Lakshya Gupta <lakshyagupta916640@gmail.com>
2025-09-12 17:35:44 +0530

    adding another section for benefits

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import Navbar from "./components/Navbar";
 import Intro from "./components/Intro";
 import Courses from "./components/Courses";
+import AdditionalBelefits from "./components/AdditionalBenefits";
 function App() {
   return (
     <div>
       <Navbar />
       <Intro />
       <Courses />
+      <AdditionalBelefits />
     </div>
   );
 }
---
+++ src/components/AdditionalBenefits.jsx
@@ -0,0 +1,37 @@
+export default function AdditionalBelefits() {
+  return (
+    <section className="container">
+      <div className="web-content">
+        <div className="benefits-container">
+          <img
+            src="https://asset.allen.in/35c08910-4ef3-4ca4-9979-2a8f7e77e7d7/original.webp?__ar__=0.997506"
+            alt=""
+          />
+          <div className="benefits-right-container">
+            <p className="benefits-heading">and Added Benefits</p>
+            <ul className="benefits-list">
+              <li className="benefits-list-item">
+                School like discipline and homework
+              </li>
+              <li className="benefits-list-item">
+                Daily Doubt Solving with teachers
+              </li>
+              <li className="benefits-list-item">
+                Best in class Video Library, Notes, Q Banks
+              </li>
+              <li className="benefits-list-item">
+                All India Test Series with analysis
+              </li>
+              <li className="benefits-list-item">
+                1-1 Mentoring sessions for students
+              </li>
+              <li className="benefits-list-item">
+                Regular PTMs and Progress Reports
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
